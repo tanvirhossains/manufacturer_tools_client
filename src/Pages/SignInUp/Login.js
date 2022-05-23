00c3791cc0f31c82bd7dc86cabd00785dafb450b
tanvirhossains <tanvirhossaincou2020@gmail.com>
2022-05-23 18:36:43 +0600

    register created

--- src/Pages/SignInUp/Login.js
+++ src/Pages/SignInUp/Login.js
@@ -1,6 +1,9 @@
 import React from 'react';
-import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth'
+import { useSignInWithEmailAndPassword, useSignInWithGoogle } from 'react-firebase-hooks/auth'
+import { Link,  } from 'react-router-dom';
+import { toast } from 'react-toastify';
 import auth from '../../firebase.init';
+import Loading from '../../Shared/Loading';
 const Login = () => {
 
     const [
@@ -9,27 +12,55 @@ const Login = () => {
         loading,
         error,
     ] = useSignInWithEmailAndPassword(auth);
+    const [signInWithGoogle, gUser, gLoading, gError] = useSignInWithGoogle(auth);
+
+    let setError
 
     const handleSubmit = event => {
+        event.preventDefault()
         const email = event.target.email.value
         const password = event.target.password.value
 
         signInWithEmailAndPassword(email, password)
+        toast.success('You are log in')
+
+    }
+    if (error || gError) {
+        setError = <p>Error: {error?.message}</p>
     }
 
+    if (loading || gLoading) {
+        return <Loading></Loading>
+    }
+
+
     return (
-        <div>
-            <h1 className=' '>login come pages</h1>
+        <div className=''>
 
-      
-            <from onSubmit={handleSubmit} action="" >
-                <input  type="email" name='email' placeholder="Type here" class="input input-bordered input-primary w-full max-w-xs" />
-                <br />
-                <input  type="password" name='password' placeholder="Type here" class="input input-bordered input-primary w-full max-w-xs my-4" />
-                <br />
+            <div className=' h-screen flex justify-center items-center '>
+                <div className="cart shadow-xl p-10 rounded-lg ">
+                    <h1 className='text-3xl '>Please Login  </h1>
 
-                <input className='btn' type="submit" value="Log in " />
-            </from>
+                    <form onSubmit={handleSubmit} action="" >
+                        <label class="label">
+                            <span class="label-text">Give Your Email Please</span>
+                        </label>
+                        <input type="email" name='email' placeholder="Type Your Email" class="input input-bordered input-primary w-full " />
+                        <br />
+                        <label class="label">
+                            <span class="label-text">Give Your Password Please</span>
+                        </label>
+                        <input type="password" name='password' placeholder="Type here" class="input input-bordered input-primary w-full  my-4" />
+                        <br />
+                        <p className='text-1xl text-red-500 font-bold'> {setError}</p>
+                        <input className='btn w-full' type="submit" value="Log in " />
+                    </form>
+
+                    <h1>Do you want to register? <Link to='/register' className='text-2xl text-orange-500'> Register</Link></h1>
+                    <div class="divider">OR</div>
+                    <button className='btn btn-outline w-full' onClick={() => signInWithGoogle()}>Sign In</button>
+                </div>
+            </div>
         </div>
     );
 };
--- src/Pages/SignInUp/Register.js
+++ src/Pages/SignInUp/Register.js
@@ -1,9 +1,64 @@
 import React from 'react';
+import { useCreateUserWithEmailAndPassword, useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import { Link } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import auth from '../../firebase.init';
 
 const Register = () => {
+    const [
+        createUserWithEmailAndPassword,
+        user,
+        loading,
+        error,
+    ] = useCreateUserWithEmailAndPassword(auth);
+
+    const [signInWithGoogle, gUser, gLoading, gError] = useSignInWithGoogle(auth);
+
+    let setError
+
+    const handleSubmit = event => {
+        const name = event.target.name.value
+        const email = event.target.email.value
+        const password = event.target.password.value
+
+        createUserWithEmailAndPassword(email, password)
+        toast.success('You are Registered successfully')
+    }
+    if (error || gError) {
+        setError = <p>Error: {error?.message}</p>
+    }
+
     return (
-        <div>
-            <h1>Register page</h1>
+        <div className=''>
+
+            <div className=' h-screen flex justify-center items-center '>
+                <div className="cart shadow-xl p-10 rounded-lg ">
+                    <h1 className='text-3xl '>Please Register  </h1>
+
+                    <form onSubmit={handleSubmit} action="" >
+                        <label class="label">
+                            <span class="label-text">Give Your Name Please</span>
+                        </label>
+                        <input type="name" name='name' placeholder="Type Your Name" class="input input-bordered input-primary w-full " />
+                        <label class="label">
+                            <span class="label-text">Give Your Email Please</span>
+                        </label>
+                        <input type="email" name='email' placeholder="Type Your Email" class="input input-bordered input-primary w-full " />
+                        <br />
+                        <label class="label">
+                            <span class="label-text">Give Your Password Please</span>
+                        </label>
+                        <input type="password" name='password' placeholder="Type here" class="input input-bordered input-primary w-full  my-4" />
+                        <br />
+                        <p className='text-1xl text-red-500 font-bold'> {setError}</p>
+                        <input className='btn w-full' type="submit" value="Log in " />
+                    </form>
+
+                    <h1>Have an account? <Link to='/login' className='text-2xl text-orange-500'> Log in</Link></h1>
+                    <div class="divider">OR</div>
+                    <button className='btn btn-outline w-full' onClick={() => signInWithGoogle()}>Sign In</button>
+                </div>
+            </div>
         </div>
     );
 };
