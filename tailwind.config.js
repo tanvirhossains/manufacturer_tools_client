db2360ba1f909c1951af0a791107d822a74edc43
tanvirhossains <tanvirhossaincou2020@gmail.com>
2022-05-23 11:22:14 +0600

    inital setup with tailwind and daisyUi

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+}
+
+
+// module.exports = {
+//   //...
+//   plugins: [require("daisyui")],
+// }
