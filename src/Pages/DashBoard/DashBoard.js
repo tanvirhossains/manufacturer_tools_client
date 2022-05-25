f5e56a7c0864b94750adab8141810aba4d307977
tanvirhossains <tanvirhossaincou2020@gmail.com>
2022-05-25 09:16:47 +0600

    purchase page order input solve

--- src/Pages/DashBoard/DashBoard.js
+++ src/Pages/DashBoard/DashBoard.js
@@ -4,16 +4,12 @@ import { NavLink, Outlet } from 'react-router-dom';
 const DashBoard = () => {
     return (
         <div>
-          
-
-
-            <div class="drawer drawer-end">
+            <div class="drawer  lg:drawer-mobile">
                 <input id="tools-dashboard" type="checkbox" class="drawer-toggle" />
                 <div class="drawer-content bg-slate-300">
                     {/* <!-- Page content here --> */}
                     <h1 className='Text-center text-white text-center p-5 font-bold text-2xl w-full bg-slate-600'> Dashboard</h1>
                     <Outlet></Outlet>
-                  
 
                 </div>
                 <div class="drawer-side">
@@ -24,7 +20,6 @@ const DashBoard = () => {
                         <NavLink to='/dashboard/addReview'>Add A Review</NavLink>
                         <NavLink to='/dashboard/myOrders'>My Orders</NavLink>
                     </ul>
-
                 </div>
             </div>
         </div>
--- src/Pages/Purchase/Purchase.js
+++ src/Pages/Purchase/Purchase.js
@@ -1,12 +1,15 @@
 import React, { useEffect, useState } from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
+import { useForm } from 'react-hook-form';
 import { useParams } from 'react-router-dom';
 import auth from '../../firebase.init';
 
 const Purchase = () => {
     const { toolId } = useParams()
-
     const [user] = useAuthState(auth)
+    const {
+        register, handleSubmit, watch, formState: { errors } } = useForm();
+
 
     console.log(user)
     const [tool, setTool] = useState({})
@@ -18,6 +21,13 @@ const Purchase = () => {
 
 
     const { name, img, quantity, minOrder } = tool
+
+    const onSubmit = (data) => {
+        alert(JSON.stringify(data));
+    }
+    // const order =
+    // if (quantity < )
+
     return (
         <div className='container mx-7'>
             <h1 className='text-3xl text-cyan-500 font-bold text-center m-5'>Purchase page</h1>
@@ -30,7 +40,8 @@ const Purchase = () => {
                         <div className='m-4'>
                             <h1 className='text-2xl font-bold text-blue-400'>Name: {name}</h1>
                             <h1 className='text-1xl font-bold text-red-400'>Minimum Order: {minOrder} </h1>
-                            <h1>Available quantity : <span className='text-red-600 font-bold'>({quantity} in stoke )</span></h1>
+                            <h1>Available quantity : {quantity < 100 ? <span className='text-green-600 font-bold'>({quantity} in stoke )</span> :
+                                <span> {quantity}</span>}</h1>
                         </div>
                     </div>
                 </div>
@@ -39,13 +50,34 @@ const Purchase = () => {
                         <div class="card-body">
                             <h2 class=" text-center text-indigo-700 font-bold text-2xl"> User Information</h2>
 
-                            <input type="text" placeholder="Type here" disabled value={user?.displayName} class="input input-bordered w-full max-w-xs" />
-                            <input type="text" placeholder="Type here" disabled value={user?.email} class="input input-bordered w-full max-w-xs" />
-                            <input type="text" placeholder="Your Address" class="input input-bordered w-full max-w-xs" />
-                            <input type="number" placeholder="Phone Number" class="input input-bordered w-full max-w-xs" />
-                            <div class="card-actions justify-center">
-                                <button class="btn btn-primary">Buy Now</button>
-                            </div>
+                            <form action="" onSubmit={handleSubmit(onSubmit)}>
+                                <input type="text" placeholder="Type here" disabled value={user?.displayName} class="input input-bordered w-full max-w-xs" />
+                                <input type="text" placeholder="Type here" disabled value={user?.email} class="input input-bordered w-full max-w-xs" />
+                                <input type="text" placeholder="Your Address" class="input input-bordered w-full max-w-xs" />
+                                <input type="phone" placeholder="Phone Number" class="input input-bordered w-full max-w-xs"
+                                {...register("phone", {
+                                    required: true,
+                                })}
+                                 />
+                                 {errors.phone?.type === 'required' && <p>Please give a Phone number</p>}
+                                
+
+
+                                <input placeholder="order" class="input input-bordered w-full max-w-xs"
+                                    {...register("number", {
+                                        required: true,
+                                        min: minOrder,
+                                        max: quantity,
+
+                                    })}
+                                />
+                                {errors?.number?.type === "required" && <p>This field is required</p>}
+                                {errors?.number?.type === "min" && (<p>please order Minimum {minOrder}</p>)}
+                                {errors?.number?.type === "max" && (<p>product {quantity}</p>)}
+
+                                <input type="submit" value="Order now " />
+                            </form>
+
                         </div>
                     </div>
                 </div>
